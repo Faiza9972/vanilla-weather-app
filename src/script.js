9a527debfe2d4d947decb99da238a621c8762396
Faiza9972 <126180982+Faiza9972@users.noreply.github.com>
2023-04-05 18:00:10 +0300

    Updated js
    Integrated js with the daily API and integrated coordinates

--- src/script.js
+++ src/script.js
@@ -22,7 +22,9 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-function displayForecast() {
+
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -50,6 +52,17 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 
 }
+
+function getForecast(coordinates) {
+console.log(coordinates);
+let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+//console.log(apiUrl);
+axios.get(apiUrl).then(displayForecast);
+}
+
+
 function displayTemperature(response) {
   //console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -74,6 +87,9 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+
 }
 function search(city) {
   let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
