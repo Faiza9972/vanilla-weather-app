c3cf090c860e4cd29d22d58f3a70ba888c71e403
Faiza9972 <126180982+Faiza9972@users.noreply.github.com>
2023-04-05 17:25:34 +0300

    Added a row for days in html and js

--- src/script.js
+++ src/script.js
@@ -22,7 +22,34 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
 
+  let forecastHTML = `<div class="row">`;
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  days.forEach(function(day) {
+    forecastHTML = forecastHTML + ` 
+  
+    <div class="col-2">
+      <div class="day">${day}</div>
+      <img
+        src="https://openweathermap.org/img/wn/50d@2x.png"
+        alt=""
+        width="40"
+      />
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">18°</span>
+        <span class="weather-forecast-temperature-min"> 12°</span>
+      </div>
+    </div>`
+;
+  });
+  
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+
+}
 function displayTemperature(response) {
   //console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -92,3 +119,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsius);
 
 search("Aspen");
+displayForecast();
